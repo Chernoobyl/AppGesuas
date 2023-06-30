8f485cb71604e44cc2dc8ddbd38a2afb46c84ab9
Rafael Camargo <37565220+Chernoobyl@users.noreply.github.com>
2023-06-30 12:42:38 -0300

    layout das demais activitys

---
+++ app/src/main/java/com/example/appgesuas/SelectServiceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appgesuas
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class SelectServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_service)
+    }
+}
